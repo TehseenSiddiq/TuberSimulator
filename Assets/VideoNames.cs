2dca73d61f55409cd15434fd03996f03c9b8a323
SpankyBoy <59478102+SpankyBoy23@users.noreply.github.com>
2021-11-25 11:43:57 +0500

    Save System And UI Animations

---
+++ Assets/VideoNames.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class VideoNames 
+{
+    public string name;
+    public int index;
+}
